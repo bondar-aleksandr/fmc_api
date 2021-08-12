9549b6f61b9f00a67774e6586bf38071acc07a5d
bondar-aleksandr <54150127+bondar-aleksandr@users.noreply.github.com>
2021-08-12 16:49:09 +0300

    fix object service

--- asa_api.py
+++ asa_api.py
@@ -35,16 +35,23 @@ class Asa:
             element = {}
             name = serv_obj.re_match_typed('^object service (\S+)')
             for child in serv_obj.children:
-                if child.re_match_typed('service (\S+)'):
-                    protocol_raw = child.re_match_typed('service (\S+)')
-                    protocol = settings.protocol_mapping[protocol_raw]
-                    element['protocol'] = protocol
+                if child.re_match_typed('description (\S+)'):
+                    continue
+                protocol = child.re_match_typed('service (\S+)')
+                if not protocol.isdigit():
+                    protocol = settings.protocol_mapping[protocol]
+                element['protocol'] = protocol
+                if protocol == '1':
+                    element['type'] = 'ICMPv4PortLiteral'
                     if child.re_match_typed('service \S+ (\S+)'):
-                        direction_raw = child.re_match_typed('service \S+ (\S+)')
-                        if direction_raw == 'destination':
-                            direction = 'dst'
-                        elif direction_raw == 'source':
-                            direction = 'src'
+                        icmp_type = child.re_match_typed('service \S+ (\S+)')
+                        if not icmp_type.isdigit():
+                            icmp_type = settings.imcp_codes[icmp_type]
+                        element['icmpType'] = icmp_type
+                else:
+                    element['type'] = 'PortLiteral'
+                    if child.re_match_typed('service \S+ (\S+)'):
+                        direction = child.re_match_typed('service \S+ (\S+)')
                         element['direction'] = direction
                         operator_ = child.re_match_typed('service \S+ \S+ (\S+)')
                         port = child.re_match_typed('service \S+ \S+ \S+ (\S+)')
@@ -63,6 +70,73 @@ class Asa:
                         element['port'] = port
             result[name] = element
         self.config_elements['object_service'] = result
+        return result
+
+    def parse_serv_obj_group(self):
+        result = {}
+
+        for serv_obj_group in self.config.find_objects(r'object-group service'):
+            serv_obj_group: IOSCfgLine
+            # for tcp/udp service-groups
+            serv_obj_gr_type = serv_obj_group.re_match_typed('^object-group service \S+ (\S+)')
+            if serv_obj_gr_type in ['tcp', 'udp', 'tcp-udp']:
+                pass
+            # for regular service-groups
+            else:
+                name = serv_obj_group.re_match_typed('^object-group service (\S+)')
+                result[name] = {}
+                result[name]['destinationPorts'] = {}
+                result[name]['destinationPorts']['literals'] = []
+                result[name]['sourcePorts'] = {}
+                result[name]['sourcePorts']['literals'] = []
+
+                for child in serv_obj_group.children:
+                    child: IOSCfgLine
+                    if child.re_match_typed('service-object object (\S+)'):
+                        serv_obj = child.re_match_typed('service-object object (\S+)')
+                        if self.config_elements['object_service'][serv_obj]['direction'] == 'destination':
+                            element = {}
+                            element['type'] = 'PortLiteral'
+                            element['protocol'] = self.config_elements['object_service'][serv_obj]['protocol']
+                            element['port'] = self.config_elements['object_service'][serv_obj]['port']
+                            result[name]['destinationPorts']['literals'].append(element)
+                        elif self.config_elements['object_service'][serv_obj]['direction'] == 'source':
+                            element = {}
+                            element['type'] = 'PortLiteral'
+                            element['protocol'] = self.config_elements['object_service'][serv_obj]['protocol']
+                            element['port'] = self.config_elements['object_service'][serv_obj]['port']
+                            result[name]['sourcePorts']['literals'].append(element)
+
+                    elif child.re_match_typed('service-object (\S+)'):
+                        element = {}
+                        protocol = settings.protocol_mapping[child.re_match_typed('service-object (\S+)')]
+                        element['type'] = 'PortLiteral'
+                        element['protocol'] = protocol
+                        if child.re_match_typed('-object \S+ (\S+)'):
+                            direction = child.re_match_typed('-object \S+ (\S+)')
+                            operator_ = child.re_match_typed('-object \S+ \S+ (\S+)')
+                            port = child.re_match_typed('-object \S+ \S+ \S+ (\S+)')
+                            if not port.isdigit():
+                                print(child)
+                                port = settings.port_mapping[port]
+                            elif operator_ == 'gt':
+                                port = f'{port}-65535'
+                            elif operator_ == 'lt':
+                                port = f'0-{port}'
+                            elif operator_ == 'range':
+                                port_start = port
+                                port_stop = child.re_match_typed('-object \S+ \S+ \S+ \S+ (\S+)')
+                                if not port_stop.isdigit():
+                                    port_stop = settings.port_mapping[port_stop]
+                                port = f'{port_start}-{port_stop}'
+                            element['port'] = port
+                            if direction == 'destination':
+                                result[name]['destinationPorts']['literals'].append(element)
+                            elif direction == 'source':
+                                result[name]['sourcePorts']['literals'].append(element)
+                            else:
+                                result[name]['destinationPorts']['literals'].append(element)
+        self.config_elements['object-group_service'] = result
         return result
 
     def parse_netw_obj(self) -> dict:
@@ -130,20 +204,8 @@ class Asa:
         self.config_elements['object-group_network'] = result
         return result
 
-    def parse_serv_obj_group(self):
-        result = {}
-        for serv_obj_group in self.config.find_objects(r'object-group service'):
-            serv_obj_group: IOSCfgLine
-            element = {}
-            name = serv_obj_group.re_match_typed('^object-group service (\S+)')
-            # for tcp/udp service-groups
-            if serv_obj_group.re_match_typed('^object-group service \S+ (\S+)'):
-                pass
-            else:
-                for child in serv_obj_group.children:
-                    child: IOSCfgLine
-                    if child.re_match_typed('service-object object (\S+)'):
-                        pass
+
+
 """
     def parse_netw_obj(self) -> list:
 
@@ -253,4 +315,5 @@ asa.read()
 asa.parse_netw_obj()
 asa.parse_netw_obj_groups()
 asa.parse_serv_obj()
+#asa.parse_serv_obj_group()
 asa.print_()
